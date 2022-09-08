eab1fcb14ab8a45cda673c270351d56e8fbad4e8
Tanmay Dixit <tanmay-dixit@outlook.com>
2022-09-08 08:44:37 +0530

    add contains duplicate

---
+++ java-solutions/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        var uniqueNumbers = new HashSet<Integer>();
+
+        for(int currentNumber: nums) {
+            if(uniqueNumbers.contains(currentNumber)) {
+                return true;
+            } else {
+                uniqueNumbers.add(currentNumber);
+            }
+        }
+
+        return false;
+
+    }
+
+}
---
+++ java-solutions/TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int currentNum;
+        int missingNum;
+        int missingNumIndex;
+        var indexByNum = new HashMap<Integer, Integer>();
+
+        for(int currentNumIndex=0; currentNumIndex<nums.length; currentNumIndex++) {
+            currentNum = nums[currentNumIndex];
+            indexByNum.put(currentNum, currentNumIndex);
+        }
+
+        for(int currentNumIndex=0; currentNumIndex<nums.length; currentNumIndex++) {
+
+            currentNum = nums[currentNumIndex];
+            missingNum = target - currentNum;
+
+            if(indexByNum.containsKey(missingNum)) {
+                missingNumIndex = indexByNum.get(missingNum);
+                if(currentNumIndex != missingNumIndex) {
+                    return new int[] { currentNumIndex , missingNumIndex };
+                }
+
+            }
+
+        }
+
+        return new int[] {-1, -1};
+
+    }
+
+}
