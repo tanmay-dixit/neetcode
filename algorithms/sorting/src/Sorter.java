068ecf623356e9b2e0c9469c5069537902e40569
Tanmay Dixit <tanmay-dixit@outlook.com>
2022-10-18 09:37:55 +0530

    add sorting algorithms

--- algorithms/sorting/src/Sorter.java
+++ algorithms/sorting/src/Sorter.java
@@ -59,20 +59,42 @@ public class Sorter {
 
             for (int j = i; j > 0; j--) {
                 System.out.println("\t Checking " + array[j - 1] + " & " + array[j]);
-
                 if (array[j] < array[j - 1]) {
                     System.out.println("\t \t ->  Swapping " + array[j - 1] + " & " + array[j]);
                     int temp = array[j - 1];
                     array[j - 1] = array[j];
                     array[j] = temp;
                 }
-
             }
-
             System.out.println("Array after iteration: " + Arrays.toString(array));
 
         }
 
+    }
+
+    public static void mergeSort(int[] array) {
+
+    }
+
+    public static void merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+
+        for (int i = 0, j = 0, index = 0; i < array1.length && j < array2.length; ) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                i++;
+            } else if (array2[j] < array1[i]) {
+                combined[index] = array2[j];
+                j++;
+            } else {
+                combined[index] = array1[i];
+                i++;
+                index++;
+                combined[index] = array2[j];
+                j++;
+            }
+            index++;
+        }
     }
 
 }
